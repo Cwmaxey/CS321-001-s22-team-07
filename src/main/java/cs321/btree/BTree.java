d7b5ea1a3524ea6dd79c17fc4024d08970c1cf88
Alex <alexliang@u.boisestate.edu>
2022-04-13 17:44:04 -0600

    Worked on TreeObject class to make a constructor using ArrayList and implemented some BTree_Search pseudocode

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -1,9 +1,13 @@
 package cs321.btree;
 
-public class BTree<E>
-{
+import java.util.Comparator;
+
+// added in Comparable - alex
+public class BTree<E>{
     // Constructor for BTree class.
-    public BTree<E>(BTree<E> T) {
+    public BTree<E>(
+    BTree<E> T)
+    {
         x = new BTree<E>(); // allocate new node
         x.leaf = true;
         x.n = 0; // number of keys in tree
@@ -22,22 +26,41 @@ public class BTree<E>
     }
 
     // Inserts into a non-full BTree TreeObject.
-    private void BTree_Insert_Nonfull(TreeObject x, int k) {
+    private void BTree_Insert_Nonfull(TreeObject x, int key) {
         return null
     }
 
     // BTree_Split_Child splits a node that is full
     // based on degree t.
     // writes to disk if successful
-    private void BTree_Split_Child(TreeObject x, int k) {
+    private void BTree_Split_Child(TreeObject x, int key) {
         return null
     }
 
     // BTree_Search looks for a node with key k in a given node x.
-    private void BTree_Search(TreeObject x, int k) {
-        return null
+    private TreeObject<E> BTree_Search(TreeObject<E> x, int key) {
+        int i = 0; // start at the root, top down search method
+        // loop thru the keys starting at the root,
+        // while the index is < than the total number of x Nodes
+        // and the key to be searched for is > than the key at index 'x'
+        // increment the search
+        while(i <= x.n && key > x.key[i]){
+            i++;
+        }
+        // if key is found return node and index
+        if(i <= x.n && key == x.key[i]){
+            return(x, key);
+        } else if(x.leaf){
+            return null;
+        } else {
+            RandomAccessFileRead();
+            return BTree_Search(x, key);
+        }
+
+
+
     }
-    
+
     // https://docs.oracle.com/javase/8/docs/api/java/io/RandomAccessFile.html#writeBytes-java.lang.String-
     public void RandomAccessFileWrite(BTree<E> T) {
         // writeBytes(String s) to write binary to a disk file.
@@ -46,8 +69,6 @@ public class BTree<E>
     // https://docs.oracle.com/javase/8/docs/api/java/io/RandomAccessFile.html#readLong--
     public long RandomAccessFileRead() {
         // btree.readLong()
-    } 
-
-
+    }
 
 }
--- src/main/java/cs321/btree/TreeObject.java
+++ src/main/java/cs321/btree/TreeObject.java
@@ -1,6 +1,25 @@
 package cs321.btree;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-public class TreeObject<E>
+/**
+ * BTree node class
+ */
+public class TreeObject<E> extends Comparable<E>
 {
+    public ArrayList<E> BTreeList;
+    int t; // degree t (max number of keys a node can have 2t-1)
+    boolean leaf; // check if node is a leaf or not
+    int n; // returns the number of keys in BTree
+    //TreeObject<E> child = new TreeObject<E>(2 * t);
+
+    // Constructor
+    public TreeObject(int t){
+        this.t = t;
+        this.leaf = leaf;
+        this.n = 0;
+        BTreeList = new ArrayList<E>((2 * t) -1);
+    }
+
 
 }
