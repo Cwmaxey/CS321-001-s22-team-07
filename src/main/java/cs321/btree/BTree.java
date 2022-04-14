470093e9fcd133d70f89217ea60885363d4f3cf3
Conner Maxey <connermaxey@u.boisestate.edu>
2022-04-14 16:16:25 -0600

    Worked on Insert BTree Nonfull, need to do unit tests still.

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -1,74 +1,142 @@
 package cs321.btree;
 
-import java.util.Comparator;
-
 // added in Comparable - alex
-public class BTree<E>{
+public class BTree {
+    public BTreeNode root; // Pointer to root node
+    public int t; // Minimum degree
+
     // Constructor for BTree class.
-    public BTree<E>(
-    BTree<E> T)
+    BTree(int t)
     {
-        x = new BTree<E>(); // allocate new node
-        x.leaf = true;
-        x.n = 0; // number of keys in tree
-        T.root = x;
-        RandomAccessFileWrite(T);
+        BTreeNode x = new BTreeNode(t, true);
+        RandomAccessFileWrite(x);
+        this.root = x;
+        this.t = t;
     }
 
-    BTree_Create(BTree <E>) {
-        x = New BTree<E>();
+    private class BTreeNode {
+        public TreeObject keys[]; // Array of keys
+        public BTreeNode C[]; // An array of child pointers
+        int t; // degree t (max number of keys a node can have 2t-1)
+        boolean leaf; // check if node is a leaf or not
+        int n; // returns the number of keys in BTree
+        
+        BTreeNode(int t, boolean leaf) {
+            this.t = t;
+            this.leaf = leaf;
+            this.C = new BTreeNode[2 * t];
+            this.keys = new TreeObject[(2 * t) - 1];
+            this.n = 0;
+        }
+
+
     }
 
     // Inserts into a BTree.
     // If full, splits the child then calls B_Tree_Insert_Nonfull.
-    private void BTree_Insert(BTree <E>, TreeObject k) {
-        return null
+    private void BTree_Insert(BTree T, TreeObject k) {
+        BTreeNode r = T.root;
+        int t = this.t;
+        if (r.n == (2*t - 1)) {
+            BTreeNode s = new BTreeNode(t, false);
+            T.root = s;
+            s.n = 0;
+            s.C[1] = r;
+            BTree_Split_Child(s, 1);
+            BTree_Insert_Nonfull(s, k.DNA);
+        } else {
+            BTree_Insert_Nonfull(r, k.DNA);
+        }
     }
 
     // Inserts into a non-full BTree TreeObject.
-    private void BTree_Insert_Nonfull(TreeObject x, int key) {
-        return null
+    private void BTree_Insert_Nonfull(BTreeNode x, long key) {
+        // The first if block handles if x is a leaf node by inserting key k into x.
+        // If x is not a leaf node then we insert k into the appropriate leaf node in the subtree
+        // that is rooted at internal node x.
+        int i = x.n;
+        if (x.leaf) {
+            while (i >= 1 && key < x.keys[i].DNA) {
+                x.keys[i+1] = x.keys[i];
+                i--;
+            }
+            x.keys[i+1].DNA = key;
+            x.n = x.n+1;
+            RandomAccessFileWrite(x);
+        } else {
+            while (i >= 1 && key < x.keys[i].DNA) {
+                i--;
+            }
+            i++;
+            RandomAccessFileRead(x.C, i);
+            if (x.C[i].n == (2*t - 1)) {
+                BTree_Split_Child(x, i);
+                if (key > x.keys[i].DNA) {
+                    i++;
+                }
+            }
+            BTreeNode child = x.C[i];
+            BTree_Insert_Nonfull(child, key);
+        }
     }
 
     // BTree_Split_Child splits a node that is full
     // based on degree t.
     // writes to disk if successful
-    private void BTree_Split_Child(TreeObject x, int key) {
-        return null
+    private void BTree_Split_Child(BTreeNode x, int key) {
+       
     }
 
     // BTree_Search looks for a node with key k in a given node x.
-    private TreeObject<E> BTree_Search(TreeObject<E> x, int key) {
-        int i = 0; // start at the root, top down search method
+    private void BTree_Search(TreeObject x, int key) {
+        // int i = 0; // start at the root, top down search method
         // loop thru the keys starting at the root,
         // while the index is < than the total number of x Nodes
         // and the key to be searched for is > than the key at index 'x'
         // increment the search
-        while(i <= x.n && key > x.key[i]){
-            i++;
-        }
-        // if key is found return node and index
-        if(i <= x.n && key == x.key[i]){
-            return(x, key);
-        } else if(x.leaf){
-            return null;
-        } else {
-            RandomAccessFileRead();
-            return BTree_Search(x, key);
-        }
-
-
+        // while(i <= x.n && key > x.key[i]){
+        //     i++;
+        // }
+        // // if key is found return node and index
+        // if(i <= x.n && key == x.key[i]){
+        //     return(x, key);
+        // } else if(x.leaf){
+        //     return null;
+        // } else {
+        //     RandomAccessFileRead();
+        //     return BTree_Search(x, key);
+        // }
 
     }
 
+    // Gets an internal node using Level Order traversal.
+    public TreeObject GetNodeAtIndex(int j) {
+        // if i < 1
+        //  return error
+        // q = newQueue()
+        // q.enQueue(root)
+        // while (!q.isEmpty())
+        //  n = q.deQueue()
+        //  if i == j
+        //      return n
+        //  else
+        //      i++
+        //  if (!n.leaf)
+        //  for each child pointer c in n
+        //      child = disk-read(c)
+        //      q.enQueue(child)
+        return null;
+    }
+
     // https://docs.oracle.com/javase/8/docs/api/java/io/RandomAccessFile.html#writeBytes-java.lang.String-
-    public void RandomAccessFileWrite(BTree<E> T) {
+    public void RandomAccessFileWrite(BTreeNode T) {
         // writeBytes(String s) to write binary to a disk file.
     }
 
     // https://docs.oracle.com/javase/8/docs/api/java/io/RandomAccessFile.html#readLong--
-    public long RandomAccessFileRead() {
+    public long RandomAccessFileRead(BTreeNode[] C, int index) {
         // btree.readLong()
+        return 123456;
     }
 
 }
