103b296272650bf7d3d51be2f3d59fa2fac3442f
jessica-lambert <63221844+jessica-lambert@users.noreply.github.com>
2022-04-17 22:17:27 -0600

    Create JUnit btreeInsertToFillRoot #18

--- src/test/java/cs321/btree/BTreeTest.java
+++ src/test/java/cs321/btree/BTreeTest.java
@@ -6,12 +6,42 @@ import static org.junit.Assert.assertEquals;
 
 public class BTreeTest
 {
-    // HINT:
+    long DNA;
+        // HINT:
     //  instead of checking all intermediate states of constructing a tree
     //  you can check the final state of the tree and
     //  assert that the constructed tree has the expected number of nodes and
     //  assert that some (or all) of the nodes have the expected values
     @Test
+    public void btreeInsertToFillRoot()
+    {
+        //using degree 2
+        BTreeNode test = new BTreeNode();
+        TreeObject A = new TreeObject(DNA);
+        TreeObject B = new TreeObject(DNA);
+        TreeObject C = new TreeObject(DNA);
+
+        test.BTree_Insert(A);
+        test.BTree_Insert(B);
+        test.BTree_Insert(c);
+        //Node should now be full and should return a size (?) of 3
+        if(test.getSize()==3)
+        {
+            assert(true);
+        }
+        else
+        {
+            assert(false);
+        }
+
+
+    }
+    
+    
+    
+    
+    @Test
+
     public void btreeDegree4Test()
     {
 //        //TODO instantiate and populate a bTree object
