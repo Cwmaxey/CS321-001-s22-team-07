b16b113434f513d1a99cd98d4770f6d8ca9a8178
jessica-lambert <63221844+jessica-lambert@users.noreply.github.com>
2022-05-06 15:33:25 -0600

    compiled

---
+++ GeneBankSearchDatabase.java
@@ -0,0 +1,13 @@
+import cs321.common.ParseArgumentException;
+import cs321.common.ParseArgumentUtils;
+
+public class GeneBankSearchDatabase
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Hello world from cs321.search.GeneBankSearchDatabase.main");
+
+    }
+
+}
---
+++ ParseArgumentException.java
@@ -0,0 +1,7 @@
+public class ParseArgumentException extends Exception
+{
+    public ParseArgumentException(String message)
+    {
+        super(message);
+    }
+}
